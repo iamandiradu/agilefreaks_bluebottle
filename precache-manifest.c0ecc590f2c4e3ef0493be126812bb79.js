d0f853eb2f2336ac0bf3ae4559e56b3cdffa504b
Radu I <radu.iamandi@itnt.ro>
2020-09-08 02:41:00 +0300

    Updates

---
+++ precache-manifest.c0ecc590f2c4e3ef0493be126812bb79.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f10ebfe78e06fb25ed7aedc03c34ed",
+    "url": "/agilefreaks_challenge/index.html"
+  },
+  {
+    "revision": "db7c62559357eb1f4b6f",
+    "url": "/agilefreaks_challenge/static/css/main.f3debe5b.chunk.css"
+  },
+  {
+    "revision": "2262031584321af8e463",
+    "url": "/agilefreaks_challenge/static/js/3.93323fe4.chunk.js"
+  },
+  {
+    "revision": "e7ce4d34784ecd087de6",
+    "url": "/agilefreaks_challenge/static/js/4.f2fb03cc.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/agilefreaks_challenge/static/js/4.f2fb03cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8884d65021cb2438ee",
+    "url": "/agilefreaks_challenge/static/js/5.bf8493fc.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/agilefreaks_challenge/static/js/5.bf8493fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87436a1d752ef43c82ef",
+    "url": "/agilefreaks_challenge/static/js/6.cfa90f3d.chunk.js"
+  },
+  {
+    "revision": "e3b93feea552d5fd932a1758b1da5f1a",
+    "url": "/agilefreaks_challenge/static/js/6.cfa90f3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db7c62559357eb1f4b6f",
+    "url": "/agilefreaks_challenge/static/js/main.a59747ce.chunk.js"
+  },
+  {
+    "revision": "aff1ef03d80a9e44b3b9",
+    "url": "/agilefreaks_challenge/static/js/runtime-main.275e4fac.js"
+  },
+  {
+    "revision": "c12bd5ecfec9e897ec30",
+    "url": "/agilefreaks_challenge/static/js/xlsx.33dbd48f.chunk.js"
+  },
+  {
+    "revision": "1170a9188a32abe2d050589bd199635b",
+    "url": "/agilefreaks_challenge/static/media/spinner.1170a918.svg"
+  }
+]);
---
+++ static/js/xlsx.33dbd48f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpblue-bottle"]=this["webpackJsonpblue-bottle"]||[]).push([[2],{293:function(t,n){},382:function(t,n){},383:function(t,n){}}]);
+//# sourceMappingURL=xlsx.33dbd48f.chunk.js.map
