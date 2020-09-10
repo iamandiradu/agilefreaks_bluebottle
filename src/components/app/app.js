94f65acf7b5bd6ad4209d81a181667de3f71323e
Radu I <radu.iamandi@itnt.ro>
2020-09-10 04:13:38 +0300

    Replace getUserCoordinates method with manual input, add useDebounce custom hook

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Switch from 'react-switch';
 import { BubbleMap, WorldMap } from '../';
-import { getUserDistance, getUserCoordinates } from '../../utils/';
+import { getUserDistance, useDebounce } from '../../utils/';
 import api from '../../api/api.js';
 import logo from '../../images/logo.png';
 import spinner from '../../images/spinner.svg';
@@ -14,23 +14,21 @@ const coffeeShopNameDelimiter = 'Blue Bottle ';
 function App() {
     const [apiToken, setApiToken] = useState('');
     const [apiData, setApiData] = useState([]);
-    const [userCoordinates, setUserCoordinates] = useState({});
+    const [userCoordinates, setUserCoordinates] = useState({
+        latitude: '',
+        longitude: '',
+    });
+    const [userCoordinatesForm, setUserCoordinatesForm] = useState({
+        latitude: '',
+        longitude: '',
+    });
     const [processedApiData, setProcessedApiData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [isMapToggled, setIsMapToggled] = useState(false);
+    const [shouldRenderMap, setShouldRenderMap] = useState(false);
 
-    // Get API Data & User coordinates
+    // Get API Data
     useEffect(() => {
-        getUserCoordinates()
-            .then((position) => {
-                setUserCoordinates({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                });
-            })
-            .catch((error) => {
-                console.error(error.message);
-            });
         api.getToken()
             .then((token) => {
                 setApiToken(token);
@@ -95,14 +93,40 @@ function App() {
 
     // Set isLoading to false if all data is loaded
     useEffect(() => {
-        if (apiToken && apiData && userCoordinates.latitude && userCoordinates.longitude) {
+        if (apiToken && apiData) {
             setIsLoading(false);
         }
-    }, [apiData, apiToken, userCoordinates.latitude, userCoordinates.longitude]);
+    }, [apiData, apiToken]);
+
+    useEffect(() => {
+        if (apiData && !isLoading) {
+            if (apiData && userCoordinates.longitude && userCoordinates.latitude && !isLoading) {
+                setShouldRenderMap(true);
+            }
+        }
+    }, [apiData, userCoordinates.latitude, isLoading, userCoordinates.longitude]);
 
     const handleMapToggle = (checked) => {
         setIsMapToggled(checked);
     };
+
+    const debouncedUserLatitude = useDebounce(userCoordinatesForm.latitude, 500);
+    const debouncedUserLongitude = useDebounce(userCoordinatesForm.longitude, 500);
+
+    useEffect(() => {
+        if (debouncedUserLatitude) {
+            setUserCoordinates((prevState) => ({
+                ...prevState,
+                latitude: debouncedUserLatitude,
+            }));
+        }
+        if (debouncedUserLongitude) {
+            setUserCoordinates((prevState) => ({
+                ...prevState,
+                longitude: debouncedUserLongitude,
+            }));
+        }
+    }, [debouncedUserLatitude, debouncedUserLongitude]);
 
     return isLoading ? (
         <div className="spinnerWrapper">
@@ -117,7 +141,25 @@ function App() {
                     <Switch onChange={handleMapToggle} checked={isMapToggled} />
                 </div>
             </header>
-            {processedApiData.length > 0 && (
+            <form>
+                <label className="formLabel">
+                    Latitude:
+                    <input
+                        type="text"
+                        name="latitude"
+                        onChange={(e) => setUserCoordinatesForm({ latitude: e.target.value })}
+                    />
+                </label>
+                <label className="formLabel">
+                    Longitude:
+                    <input
+                        type="text"
+                        name="longitude"
+                        onChange={(e) => setUserCoordinatesForm({ longitude: e.target.value })}
+                    />
+                </label>
+            </form>
+            {shouldRenderMap ? (
                 <div className="main">
                     {isMapToggled ? (
                         <WorldMap data={processedApiData} />
@@ -125,6 +167,8 @@ function App() {
                         <BubbleMap data={processedApiData} />
                     )}
                 </div>
+            ) : (
+                <p>The map will render after you complete the coordinates.</p>
             )}
         </div>
     );
