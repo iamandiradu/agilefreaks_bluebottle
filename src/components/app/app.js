893620ff4dee1a4900d4c7b1db7001956e5b1a60
Radu I <radu.iamandi@itnt.ro>
2020-09-18 22:36:56 +0300

    Export data processor as helper function

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Switch from 'react-switch';
 import { XYChart, WorldMap } from '../';
-import { getUserDistance, useDebounce } from '../../utils/';
+import { useDebounce, processMapData } from '../../utils/';
 import api from '../../api/api.js';
 import logo from '../../images/logo.png';
 import spinner from '../../images/spinner.svg';
@@ -53,54 +53,16 @@ function App() {
     // Process API Data & User coordinates
     useEffect(() => {
         if (apiData && userCoordinates.latitude && userCoordinates.longitude) {
-            let coffeeShopsData = [];
+            let coffeeShopsData = processMapData(
+                apiData,
+                userCoordinates,
+                coffeeShopsShownOnMap,
+                coffeeShopNameSplitter
+            );
 
-            if (apiData.length > 0) {
-                const userData = {
-                    label: 'User',
-                    latitude: parseFloat(userCoordinates.latitude),
-                    longitude: parseFloat(userCoordinates.longitude),
-                    color: '#ff0000',
-                    value: 1,
-                };
-
-                // Generate the coffee shops data (sort, slice, distance, tooltip)
-                coffeeShopsData = apiData
-                    .reduce((acc, coffeeShop) => {
-                        //  Create tooltip. ex: 12345.6 km
-                        const tooltip = `${getUserDistance(
-                            userCoordinates,
-                            coffeeShop.x,
-                            coffeeShop.y
-                        )} km`;
-
-                        // If a splitter text is present, remove that text from the coffee shop's name.
-                        const label = coffeeShopNameSplitter
-                            ? coffeeShop.name.split(coffeeShopNameSplitter).pop()
-                            : coffeeShop.name;
-
-                        const coffeeShopData = {
-                            label,
-                            customTooltip: tooltip,
-                            latitude: parseFloat(coffeeShop.x),
-                            longitude: parseFloat(coffeeShop.y),
-                            distance: getUserDistance(userCoordinates, coffeeShop.x, coffeeShop.y),
-                            color: '#009ED9',
-                            value: 1, // This value is required by the AmCharts package to calculate the elements radius ratio.
-                        };
-
-                        acc.push(coffeeShopData);
-                        return acc;
-                    }, [])
-                    .sort((a, b) => (a.distance > b.distance ? 1 : -1))
-                    .slice(0, coffeeShopsShownOnMap);
-
-                // Push user data into data array
-                coffeeShopsData.push(userData);
-            }
             setProcessedApiData(coffeeShopsData);
         }
-    }, [apiData, userCoordinates, userCoordinates.latitude, userCoordinates.longitude]);
+    }, [apiData, userCoordinates]);
 
     // Set isLoading to false if all data is loaded
     useEffect(() => {
