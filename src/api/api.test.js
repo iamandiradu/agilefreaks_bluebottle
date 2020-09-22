a56243a4a4b10b00c75844c970b3a2af9bf15bfa
Radu I <radu.iamandi@itnt.ro>
2020-09-23 01:23:42 +0300

    Add API test seeds, improve api tests - wip

--- src/api/api.test.js
+++ src/api/api.test.js
@@ -1,4 +1,5 @@
 import api from './api';
+import seeds from './test-seeds';
 import urls from './urls';
 import axios from 'axios';
 
@@ -6,23 +7,74 @@ jest.mock('axios');
 
 describe('api', () => {
     it('should get the token', async () => {
-        axios.post = jest.fn(() => ({ data: { token: '123' } }));
+        axios.post.mockResolvedValue({
+            data: {
+                token: seeds.token,
+            },
+        });
         const token = await api.getToken();
         expect(axios.post).toHaveBeenCalledTimes(1);
-        expect(axios.post).toHaveBeenCalledWith(`${urls.apiURL}/tokens`);
-        expect(token).toEqual('123');
+        expect(axios.post).toHaveBeenCalledWith(`${urls.apiURL}/tokens`, {
+            timeout: seeds.timeout,
+        });
+        expect(token).toEqual(seeds.token);
     });
 
     it('should get the data', async () => {
-        axios.get = jest.fn(() => ({ data: 'coffeeShops' }));
-        const data = await api.getData(123);
+        axios.get.mockResolvedValue({
+            data: seeds.coffeeShopData,
+        });
+        const data = await api.getData(seeds.token);
         expect(axios.get).toHaveBeenCalledTimes(1);
-        expect(axios.get).toHaveBeenCalledWith(`${urls.apiURL}/coffee_shops?token=123`, {
+        expect(axios.get).toHaveBeenCalledWith(`${urls.apiURL}/coffee_shops?token=${seeds.token}`, {
             headers: {
                 Accept: 'application/json',
             },
+            timeout: seeds.timeout,
         });
 
-        expect(data).toEqual('coffeeShops');
+        expect(data).toEqual(seeds.coffeeShopData);
+    });
+
+    // Timeout
+    it('should timeout', async () => {
+        axios.get.mockRejectedValueOnce(seeds.mockTimeoutError);
+        await api.getData(seeds.token).catch((error) => {
+            expect(error).toEqual(seeds.mockTimeoutError);
+        });
+        expect(axios.get).toHaveBeenCalledWith(`${urls.apiURL}/coffee_shops?token=${seeds.token}`, {
+            headers: {
+                Accept: 'application/json',
+            },
+            timeout: 1000,
+        });
+    });
+
+    // 401 Unauthorized
+    it('should 401', async () => {
+        axios.get.mockRejectedValueOnce(seeds.mock401Error);
+        await api.getData(seeds.token).catch((error) => {
+            expect(error).toEqual(seeds.mock401Error);
+        });
+        expect(axios.get).toHaveBeenCalledWith(`${urls.apiURL}/coffee_shops?token=${seeds.token}`, {
+            headers: {
+                Accept: 'application/json',
+            },
+            timeout: 1000,
+        });
+    });
+
+    // 503 Service unavailable
+    it('should 503', async () => {
+        axios.get.mockRejectedValueOnce(seeds.mock503Error);
+        await api.getData(seeds.token).catch((error) => {
+            expect(error).toEqual(seeds.mock503Error);
+        });
+        expect(axios.get).toHaveBeenCalledWith(`${urls.apiURL}/coffee_shops?token=${seeds.token}`, {
+            headers: {
+                Accept: 'application/json',
+            },
+            timeout: 1000,
+        });
     });
 });
