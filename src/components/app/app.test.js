4c073f15e2e379a0e247c5906c77b4b80e51e133
Radu I <radu.iamandi@itnt.ro>
2020-09-25 00:53:41 +0300

    Improve app tests, add seeds file

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -4,6 +4,7 @@ import { act } from 'react-dom/test-utils';
 
 import App from './App';
 import api from '../../api/api';
+import seeds from './test-seeds';
 
 jest.mock('../../api/api');
 jest.mock('../world-map/world-map.js');
@@ -12,25 +13,17 @@ jest.mock('../xy-chart/xy-chart.js');
 describe('App', () => {
     let app;
 
-    beforeEach(async () => {
-        api.getToken = jest.fn(() => Promise.resolve(123));
-        api.getData = jest.fn(() => Promise.resolve([]));
-        await act(async () => {
-            app = await mount(<App />);
-        });
-        app.update();
-    });
-
     it('should show loading spinner while loading data', async () => {
         api.getToken = jest.fn(() => Promise.resolve());
         api.getData = jest.fn(() => Promise.resolve());
         await act(async () => {
             app = await mount(<App />);
         });
+        app.update();
         expect(app.exists('.spinnerWrapper')).toBeTruthy();
         expect(app.exists('.header')).toBeFalsy();
         expect(app.exists('.formWrapper')).toBeFalsy();
-        api.getToken = jest.fn(() => Promise.resolve(123));
+        api.getToken = jest.fn(() => Promise.resolve(seeds.token));
         api.getData = jest.fn(() => Promise.resolve([]));
         await act(async () => {
             app = await mount(<App />);
@@ -61,11 +54,11 @@ describe('App', () => {
     it('should not render map if only 1 coordinate is present', () => {
         act(() => {
             app.find('.latitudeLabel input').simulate('change', {
-                target: { value: '45.45' },
+                target: { value: seeds.userCoordinates.latitude },
             });
         });
         app.update();
-        expect(app.find('.latitudeLabel input').prop('value')).toBe('45.45');
+        expect(app.find('.latitudeLabel input').prop('value')).toBe(seeds.userCoordinates.latitude);
         expect(app.exists('#xy-graph')).toBeFalsy();
         expect(app.exists('#world-map')).toBeFalsy();
     });
@@ -73,12 +66,14 @@ describe('App', () => {
     it('should render XY Graph if both coordinates are present', async () => {
         act(() => {
             app.find('.longitudeLabel input').simulate('change', {
-                target: { value: '54.54' },
+                target: { value: seeds.userCoordinates.longitude },
             });
         });
         app.update();
 
-        expect(app.find('.longitudeLabel input').prop('value')).toBe('54.54');
+        expect(app.find('.longitudeLabel input').prop('value')).toBe(
+            seeds.userCoordinates.longitude
+        );
 
         const waitForXYGraph = await app.find('main#xy-graph');
         expect(waitForXYGraph).toBeTruthy();
@@ -98,5 +93,38 @@ describe('App', () => {
     it('should render WorldMap', async () => {
         const waitForWorldMap = await app.find('main#world-map');
         expect(waitForWorldMap).toBeTruthy();
+    });
+});
+
+describe('App Error Handling', () => {
+    let app;
+
+    it('should show loading spinner while loading data', async () => {
+        api.getToken = jest.fn(() => Promise.resolve());
+        api.getData = jest.fn(() => Promise.resolve());
+        await act(async () => {
+            app = await mount(<App />);
+        });
+        app.update();
+
+        expect(app.exists('.spinnerWrapper')).toBeTruthy();
+        expect(app.exists('.header')).toBeFalsy();
+        expect(app.exists('.formWrapper')).toBeFalsy();
+    });
+    it('should show Error screen on API error', async () => {
+        api.getToken = jest.fn(() => Promise.resolve(seeds.token));
+        api.getData = jest.fn().mockImplementation(
+            () =>
+                new Promise((resolve, reject) => {
+                    reject();
+                })
+        );
+        await act(async () => {
+            app = await mount(<App />);
+        });
+        app.update();
+
+        expect(app.exists('.spinnerWrapper')).toBeFalsy();
+        expect(app.exists('.errorScreen')).toBeTruthy();
     });
 });
